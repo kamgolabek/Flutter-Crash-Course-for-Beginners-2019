455294a6859b8389a8de87ef39f06bde9d9495f6
Kamil Gołąbek <kamgolabek@gmail.com>
2020-01-15 23:12:25 +0100

    button onPress

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -22,15 +26,15 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
