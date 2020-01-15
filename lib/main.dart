4186a6de68d624a676dc9babe17c50f583ad688c
Kamil Gołąbek <kamgolabek@gmail.com>
2020-01-15 23:37:07 +0100

    stateful widget transform

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,23 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex += 1;
+    setState(() {
+       questionIndex += 1;
+    });
+   
   }
 
   @override
