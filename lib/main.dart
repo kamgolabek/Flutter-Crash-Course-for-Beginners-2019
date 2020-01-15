ae55f177aaafeb2aab2c8f74113b1edd28fdbdfc
Kamil Gołąbek <kamgolabek@gmail.com>
2020-01-15 23:48:40 +0100

    more widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,16 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-       questionIndex += 1;
+       _questionIndex += 1;
     });
    
   }
@@ -37,7 +37,7 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Text(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
